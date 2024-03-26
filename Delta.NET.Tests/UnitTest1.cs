051a0606113c8a9a6152c5f0f5132d29a19bf22e
ROUSSIN Benjamin <benroussin@hotmail.com>
2024-03-26 15:21:05 -0300

    Added Delete 1 test

--- Delta.NET.Tests/UnitTest1.cs
+++ Delta.NET.Tests/UnitTest1.cs
@@ -10,6 +10,14 @@ namespace DeltaNET.Tests
         {
             var Json = @"{""ops"":[{""retain"":1,""attributes"":{""align"":null}}]}";
             var Delta = JsonSerializer.Deserialize<Delta>(Json);
-        }
+        }  
+        [TestMethod]
+        public void Operation()
+        {
+            var Json = @"{""ops"":[{""insert"":""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."",""attributes"":{""background"":""#556271"",""color"":""#7b8898""}}]}";
+            var Delta = JsonSerializer.Deserialize<Delta>(Json);
+            var Delta2 = new Delta { Operations = new() { new DeleteOperation { Value = 1 } } };
+            var Result = Delta.Compose(Delta2);
+        }  
     }
 }
--- Delta.NET/Delta.cs
+++ Delta.NET/Delta.cs
@@ -13,8 +13,10 @@ namespace DeltaNET
             var NewDelta = new Delta() { Operations = new() };
             var ItMine = new OpIterator(Operations);
             var ItOther = new OpIterator(Other.Operations);
+            var DoOnce = false;
             if (ItOther.Current != null && ItOther.Current is RetainOperation retain)
             {
+                //Faster this way to deal with the first retain operation
                 var FirstLeft = retain.Value;
                 while (ItMine.Current != null && ItMine.Current is InsertOperation insert && FirstLeft >= insert.GetLength())
                 {
@@ -23,44 +25,51 @@ namespace DeltaNET
                     ItMine.Next();
                 }
                 if (retain.Value - FirstLeft > 0)
-                {
                     ItOther.Next(retain.Value - FirstLeft);
-                }
-                while (ItMine.HasNext() || ItOther.HasNext())
+            }
+            else
+            {
+                //Event if there is no 'next' operation, we need to check the first one
+                DoOnce = true;
+            }
+            while (ItMine.HasNext() || ItOther.HasNext() || DoOnce)
+            {
+                DoOnce = false;
+                if (ItOther.Current is InsertOperation)
                 {
-                    if (ItOther.Current is InsertOperation)
+                    NewDelta.Operations.Add(ItOther.Current.Clone());
+                    ItOther.Next();
+                }
+                else if (ItMine.Current is DeleteOperation)
+                {
+                    NewDelta.Operations.Add(ItMine.Current.Clone());
+                    ItMine.Next();
+                }
+                var SharedLength = Math.Min(ItMine.PeekLength(), ItOther.PeekLength());
+                var MineOp = ItMine.Next(SharedLength);
+                var OtherOp = ItOther.Next(SharedLength);
+                if (OtherOp is RetainOperation rOther)
+                {
+                    if (MineOp is RetainOperation rMine)
                     {
-                        NewDelta.Operations.Add(ItOther.Current.Clone());
-                        ItOther.Next();
+                        rMine.Value = SharedLength;
+                        NewDelta.Operations.Add(rMine);
                     }
-                    else if (ItMine.Current is DeleteOperation)
+                    else
+                        NewDelta.Operations.Add(MineOp);
+                }
+                if (!ItOther.HasNext())
+                {
+                    //We can escape here by dealing the end of Mine operations
+                    if (ItMine.Offset != 0)
                     {
-                        NewDelta.Operations.Add(ItMine.Current.Clone());
-                        ItMine.Next();
+                        NewDelta.Operations.Add(ItMine.Next());
                     }
-                    var SharedLength = Math.Min(ItMine.PeekLength(), ItOther.PeekLength());
-                    var MineOp = ItMine.Next(SharedLength);
-                    var OtherOp = ItOther.Next(SharedLength);
-                    Operation NewOp = null;
-                    if (OtherOp is RetainOperation rOther)
-                    {
-                        if (MineOp is RetainOperation rMine)
-                        {
-                            rMine.Value = SharedLength;
-                            NewDelta.Operations.Add(rMine);
-                        }
-                        else
-                        {
-                            NewDelta.Operations.Add(MineOp);
-                        }
-                    }
-                    if (!ItOther.HasNext() && ItMine.Offset == 0 && !(OtherOp is DeleteOperation))
-                    {
-                        NewDelta.Operations.AddRange(ItMine.Rest());
-                        break;
-                    }
+                    NewDelta.Operations.AddRange(ItMine.Rest());
+                    break;
                 }
             }
+
             return NewDelta;
         }
 
